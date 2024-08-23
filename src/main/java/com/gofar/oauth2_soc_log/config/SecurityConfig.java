173014a910df77499f66fe2e441d3608dffd5cef
Tountoun AYANOU <tountoun.ayanou@ace3i.com>
2024-08-23 18:55:17 +0000

    config clients in property file

--- src/main/java/com/gofar/oauth2_soc_log/config/SecurityConfig.java
+++ src/main/java/com/gofar/oauth2_soc_log/config/SecurityConfig.java
@@ -5,10 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,26 +18,6 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .oauth2Login(Customizer.withDefaults());
         return http.build();
-    }
-
-    @Bean
-    ClientRegistrationRepository clientRegistrationRepository() {
-        return new InMemoryClientRegistrationRepository(
-                githubClientRegistration(),
-                facebookClientRegistration()
-        );
-    }
-
-    private ClientRegistration githubClientRegistration() {
-        return CommonOAuth2Provider.GITHUB.getBuilder("github-client")
-                .clientId("Ov53ligvnH3iTjWTJvCR")
-                .clientSecret("b8738825a5e0258169401c31524fece416b928f9").build();
-    }
-
-    private ClientRegistration facebookClientRegistration() {
-        return CommonOAuth2Provider.FACEBOOK.getBuilder("facebook-client")
-                .clientId("470881102531759")
-                .clientSecret("d1b3cd1f2e550115a236309e6d8Bcd2c").build();
     }
 
 }
