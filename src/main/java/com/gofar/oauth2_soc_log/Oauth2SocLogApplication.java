1e5dc2b77a5d11dea98fc636934f23a4dad42811
Tountoun AYANOU <tountoun.ayanou@ace3i.com>
2024-08-23 14:13:10 +0000

    init project

---
+++ src/main/java/com/gofar/oauth2_soc_log/Oauth2SocLogApplication.java
@@ -0,0 +1,13 @@
+package com.gofar.oauth2_soc_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2SocLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2SocLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gofar/oauth2_soc_log/Oauth2SocLogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gofar.oauth2_soc_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2SocLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
